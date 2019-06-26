0c1a842df6cd73a6d34f5ecd09a94bbdd526724e
IvanSimcic <ivan.odbojkas.10@gmail.com>
2019-06-26 11:49:30 +0200

    Added finder for functions.

--- finder.py
+++ finder.py
@@ -9,31 +9,34 @@ import re
 
 def get_mod(filename):
   modules = []
-  pattern = re.compile(".+?(?=_Mod$)", re.IGNORECASE)
+  pattern = re.compile(".+?(?=_Mod$)", re.IGNORECASE)  #search for pattern
 
   with open (filename, 'rt') as myfile:
-      for line in myfile:
-          if pattern.search(line) != None:
-              modules.append(( line.rstrip("\n")))
+    for line in myfile:
+      if pattern.search(line) != None:
+        modules.append(( line.rstrip("\n")))
 
   modules = [s.strip() for s in modules if s.strip()] # remove whitespaces
   mod_list = ' '.join(modules)                        # class into a list
   module_name = re.sub("module ", "", mod_list)       # remove "module"
-  print("Module: ",module_name)
 
-#get_mod("Mesh_Mod.f90")
+  print("Module: ",module_name)
+  #return module_name
+
 
 #===============================================================================
 # Finding variables
 #-------------------------------------------------------------------------------
+
 def get_var(filename):
 
 # finding variables
+
   vars = []
   with open(filename) as file:
     for line in file:
-      urls = re.findall("(?<=:: ).*$", line)
-      vars.append(urls)
+      vars_help = re.findall("(?<=:: ).*$", line)
+      vars.append(vars_help)
   vars2 = [x for x in vars if x != []]
 
   flat_list = []
@@ -51,23 +54,46 @@ def get_var(filename):
 
   with open (filename, 'rt') as myfile:
     for line in myfile:
-      if pattern.search(line) != None:      # if a match is found
+      if pattern.search(line) != None:
         var_type.append(( line.rstrip("\n")))
 
-  var_type = [s.strip() for s in var_type if s.strip()] # getting rid of whitespaces
+  var_type = [s.strip() for s in var_type if s.strip()] # remove whitespaces
   var_type_list = [i.split()[0] for i in var_type]
   var_type_list = ([s.strip(',') for s in var_type_list])
 
 # merge into one list
 
-  result_list = [var_type_list[i] + var_list[i] for i in range(len(var_type_list))]
-  print("Variables: ", result_list[:])
+  var_result_list = [var_type_list[i] + var_list[i] \
+                    for i in range(len(var_type_list))]
 
-#get_var("Eddy_Mod.f90")
+  print("Variables: ", var_result_list[:])
 
 
 #===============================================================================
-# Finding module and variable function
+# Finding functions
+#-------------------------------------------------------------------------------
+
+def get_function(filename):
+
+  functions = []
+  with open(filename) as file:
+    for line in file:
+      funs = re.findall("(?<=_Mod/)(.*)(?=.f90)", line)
+      functions.append(funs)
+  functions2 = [x for x in functions if x != []]
+
+  flat_list = []
+  for sublist in functions2:
+    for item in sublist:
+      flat_list.append(item)
+
+  fun_list = [i.split()[0] for i in flat_list]
+
+  print("Functions: ", fun_list[:])
+
+
+#===============================================================================
+# Finding all function
 #-------------------------------------------------------------------------------
 
 def get_all(filename):
@@ -76,5 +102,6 @@ def get_all(filename):
 
   get_var(filename)
 
+  get_function(filename)
 
 get_all("Mesh_Mod.f90")
