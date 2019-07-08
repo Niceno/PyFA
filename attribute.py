0ad58b6f9618c7d5a1214efb2740617573e5ba13
IvanSimcic <ivan.odbojkas.10@gmail.com>
2019-07-08 13:12:19 +0200

    Further development.

--- attribute.py
+++ attribute.py
@@ -4,7 +4,7 @@
 import xfig
 import finder
 import browse
-
+import os
 #===============================================================================
 # Check if use list is empty
 #-------------------------------------------------------------------------------
@@ -20,32 +20,34 @@ def check_use(list):
 # Define module class
 #-------------------------------------------------------------------------------
 class Module(object):
-  def __init__(module, name, use, var, meth):
-    module.name = name
-    module.use  = use
-    module.var  = var
-    module.meth = meth
+  def __init__(module, name, use, var, meth, level):
+    module.name  = name
+    module.use   = use
+    module.var   = var
+    module.meth  = meth
+    module.level = level
 
   def print_it(abc):
     print("\nModule name: ", abc.name,     \
           "\n\nUse : ",      abc.use,      \
           "\n\nVariables: ", abc.var,      \
-          "\n\nMethods: ",   abc.meth)
+          "\n\nMethods: ",   abc.meth,     \
+          "\n\nLevel: ",     abc.level)
 
 #===============================================================================
 # Define subroutine class
 #-------------------------------------------------------------------------------
 class Subroutine(object):
-  def __init__(subroutine, name, use, var):
+  def __init__(subroutine, name, use, var, level):
     subroutine.name = name
     subroutine.use  = use
     subroutine.var  = var
-
+    subroutine.level = level
   def print_it(abc):
     print("\nSubroutine name: ", abc.name, \
           "\n\nUse : ",          abc.use,  \
-          "\n\nVariables: ",     abc.var)
-
+          "\n\nVariables: ",     abc.var,  \
+          "\n\nLevel: ",         abc.level)
 #===============================================================================
 # Define
 #-------------------------------------------------------------------------------
@@ -53,9 +55,22 @@ def module_class(filename):
 
   module_name  = finder.get_mod(filename)
   use_list     = check_use(finder.get_use(filename))
+
   var_list     = finder.get_var(filename)
   meth_list    = finder.get_meth(filename)
-  modules_list = Module(module_name, use_list, var_list, meth_list)
+
+  if use_list == "None":
+    level = 0
+  elif use_list != "None":
+    level = "?"
+
+  if use_list =="None":
+    use_list = "None"
+  else:
+    use_list = [i.split()[1] for i in use_list]           # take use name
+    use_list = ([s.strip(",") for s in use_list])         # remove ","
+
+  modules_list = Module(module_name, use_list, var_list, meth_list, level)
 
   return modules_list
 
@@ -65,7 +80,19 @@ def subroutine_class(filename):
   sub_name   = finder.get_sub(filename)
   use_list   = check_use(finder.get_use(filename))
   var_list   = finder.get_var(filename)
-  subroutine = Subroutine(sub_name, use_list, var_list)
+
+  if use_list == []:
+    level = 0
+  else:
+    level = "?"
+
+  if use_list =="None":
+    use_list = "None"
+  else:
+    use_list = [i.split()[1] for i in use_list]           # take use name
+    use_list = ([s.strip(",") for s in use_list])         # remove ","
+
+  subroutine = Subroutine(sub_name, use_list, var_list, level)
 
   return subroutine
 
@@ -73,7 +100,7 @@ def subroutine_class(filename):
 # Collecting classes into lists
 #-------------------------------------------------------------------------------
 
-root = "/home/simcic/Development/PyFA"
+root = "/home/simcic/Development/Synthetic-Eddies"
 
 files = browse.source_files(root)        # list of all fortran files in root
 subroutines_list = []
@@ -89,5 +116,25 @@ for i in range(len(files)):
   else:
     subroutines_list.append(subroutine_class(files[i]))
 
-print(subroutines_list[1].name)
-print(modules_list[1].name)
+"""
+name_list = []
+for i in range(len(modules_list)):
+  name_list.append(modules_list[i].name)
+
+  if modules_list[i].level != 0:
+
+    use_list = modules_list[i].use
+    modules_list[i].level =  "?"
+
+    print(modules_list[i].name,modules_list[i].level)
+
+print("Imena: ",name_list)
+"""
+
+
+#print(subroutines_list[2].name)
+print("\nModule name: ",modules_list[2].name,"\nModules used: ",modules_list[2].use,\
+"\nLevel: ",modules_list[2].level)
+
+print("\nModule name: ",modules_list[1].name,"\nModules used: ",modules_list[1].use,\
+"\nLevel: ",modules_list[1].level)
