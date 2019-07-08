990c84c67bff83880bb0d08415167b556b5505e7
IvanSimcic <ivan.odbojkas.10@gmail.com>
2019-07-08 16:22:10 +0200

    Further development.

--- attribute.py
+++ attribute.py
@@ -39,9 +39,9 @@ class Module(object):
 #-------------------------------------------------------------------------------
 class Subroutine(object):
   def __init__(subroutine, name, use, var, level):
-    subroutine.name = name
-    subroutine.use  = use
-    subroutine.var  = var
+    subroutine.name  = name
+    subroutine.use   = use
+    subroutine.var   = var
     subroutine.level = level
   def print_it(abc):
     print("\nSubroutine name: ", abc.name, \
@@ -49,30 +49,27 @@ class Subroutine(object):
           "\n\nVariables: ",     abc.var,  \
           "\n\nLevel: ",         abc.level)
 #===============================================================================
-# Define
+# Import attributes from fortran file and return filled in class
 #-------------------------------------------------------------------------------
 def module_class(filename):
 
   module_name  = finder.get_mod(filename)
   use_list     = check_use(finder.get_use(filename))
-
   var_list     = finder.get_var(filename)
   meth_list    = finder.get_meth(filename)
 
-  if use_list == "None":
+  if use_list == "None":        # if use list is empty,module level is 0
     level = 0
-  elif use_list != "None":
-    level = "?"
+  elif use_list != "None":      # if use list is not empty,module level is "?"
+    level = 0  ## should be "?"
 
-  if use_list =="None":
-    use_list = "None"
-  else:
-    use_list = [i.split()[1] for i in use_list]           # take use name
-    use_list = ([s.strip(",") for s in use_list])         # remove ","
+  if use_list != "None":
+    use_list = [i.split()[1] for i in use_list]    # only take name of used
+    use_list = ([s.strip(",") for s in use_list])  # modules without other info
 
-  modules_list = Module(module_name, use_list, var_list, meth_list, level)
+  modules = Module(module_name, use_list, var_list, meth_list, level)
 
-  return modules_list
+  return modules
 
 
 def subroutine_class(filename):
@@ -81,60 +78,67 @@ def subroutine_class(filename):
   use_list   = check_use(finder.get_use(filename))
   var_list   = finder.get_var(filename)
 
-  if use_list == []:
+  if use_list == []:           # if use list is empty,module level is 0
     level = 0
-  else:
-    level = "?"
+  else:                        # if use list is not empty,module level is "?"
+    level = 0                       ## should be "?"
 
-  if use_list =="None":
-    use_list = "None"
-  else:
-    use_list = [i.split()[1] for i in use_list]           # take use name
-    use_list = ([s.strip(",") for s in use_list])         # remove ","
+  if use_list != "None":
+    use_list = [i.split()[1] for i in use_list]    # only take name of used
+    use_list = ([s.strip(",") for s in use_list])  # modules without other info
 
   subroutine = Subroutine(sub_name, use_list, var_list, level)
 
   return subroutine
 
 #===============================================================================
+# Finding current level of module
+#-------------------------------------------------------------------------------
+def find_level(modules_list,name):
+  for i in range(len(modules_list)):
+    if modules_list[i].name == name:
+      lvl = modules_list[i].level
+  return lvl
+
+#===============================================================================
 # Collecting classes into lists
 #-------------------------------------------------------------------------------
-
 root = "/home/simcic/Development/Synthetic-Eddies"
 
 files = browse.source_files(root)        # list of all fortran files in root
-subroutines_list = []
-modules_list = []
+subroutines_list = []                    # initialize list
+modules_list     = []                    # initialize list
 
 for i in range(len(files)):
+  module_name = finder.get_mod(files[i])  # find all modules from imported files
 
-  module_name = finder.get_mod(files[i])
-
+# If it is module then append to modules list
   if module_name != []:
     modules_list.append(module_class(files[i]))
 
+# If it isn't module then append to subroutines list
   else:
     subroutines_list.append(subroutine_class(files[i]))
 
-"""
-name_list = []
+#===============================================================================
+# Determining levels of modules (not working for now)
+#-------------------------------------------------------------------------------
+
 for i in range(len(modules_list)):
-  name_list.append(modules_list[i].name)
 
-  if modules_list[i].level != 0:
+  if modules_list[i].use != "None":     # if there are use statements in module
+    use_list = modules_list[i].use      # get them
+    lvl = []
+    for k in range(len(use_list)):      # for every use in module
+      lvl.append(find_level(modules_list,use_list[k])) # find used module level
 
-    use_list = modules_list[i].use
-    modules_list[i].level =  "?"
-
-    print(modules_list[i].name,modules_list[i].level)
-
-print("Imena: ",name_list)
-"""
+    lvl = max(lvl)                    # take the biggest module level from list
+    modules_list[i].level = lvl + 1   # stopped here
+    print(use_list)
 
 
-#print(subroutines_list[2].name)
-print("\nModule name: ",modules_list[2].name,"\nModules used: ",modules_list[2].use,\
-"\nLevel: ",modules_list[2].level)
+print("\nModule name: ", modules_list[2].name, "\nModules used: ",   \
+      modules_list[2].use, "\nLevel: ", modules_list[2].level)
 
-print("\nModule name: ",modules_list[1].name,"\nModules used: ",modules_list[1].use,\
-"\nLevel: ",modules_list[1].level)
+print("\nModule name: ", modules_list[0].name, "\nModules used: ",   \
+      modules_list[0].use, "\nLevel: ", modules_list[0].level)
