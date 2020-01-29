ae848dae4f63b1954036a98b51772c31d0448cee
Niceno <bojan.niceno.scientist@gmail.com>
2020-01-29 14:19:38 +0100

    Renamed.

---
+++ Const.py
@@ -0,0 +1,22 @@
+#===============================================================================
+# Module with constants
+#-------------------------------------------------------------------------------
+BOX_MARGINS             = 1.5                  # box margins in cm (presumably)
+UNIT_BOX_HEIGHT         = 1.00                 # unit box height
+THICKNESS               = 2                    # box line thickness
+FIG_FILE_NAME           = "diagram.fig"        # xfig output file name
+IJ_FILE_NAME            = "diagram.ij"         # file with object i,j coords
+XY_FILE_NAME            = "diagram.xy"         # file with object i,j coords
+FONT_SIZE               = UNIT_BOX_HEIGHT*0.5  # font size related to box height
+FONT_HEADER             = "Courier-Bold"       # font for headers
+FONT_NORMAL             = "Courier"            # font for everything else
+COLOR_BOX               = "White"              # color of var,meth and use boxes
+COLOR_HEADER_MODULE     = "LtBlue"             # color of module header
+COLOR_HEADER_SUBROUTINE = "Pink2"              # color of subroutine header
+COLOR_HEADER_FUNCTION   = "Yellow"             # color of function header
+COLOR_HEADER_PROGRAM    = "Green2"             # color of program header
+STARTING_LAYER_USE      = 100
+STARTING_LAYER_CALL     = 200
+LAYER_GRID_CALLS        = 500
+XFIG_SCALE              = 450                  # xfig units for one cm
+COMPOUND_MARGIN         =  45
