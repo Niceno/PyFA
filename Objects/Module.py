d1a1a8715c12267059ff74ceabfa9ade70587ae3
Niceno <bojan.niceno.scientist@gmail.com>
2020-01-29 21:43:36 +0100

    All inherit from one mother.

--- Objects/Module.py
+++ Objects/Module.py
@@ -1,47 +1,21 @@
+from Objects.Object import Object  # mother
 
 #===============================================================================
-# Defining module class
+# Defining Module class
 #
 # Parameters:
-#   - module:     initialize name (e.g. you can write module.name to get name)
-#   - type:       type of object (can be module/subroutine/function/program)
-#   - name:       name of the module
-#   - use:        list of module use statements
-#   - var:        list of module variables
-#   - meth:       list of module methods
-#   - level:      level of module
-#   - x0:         first corner(upper left) position on x axis in centimeters
-#   - y0:         first corner(upper left) position on y axis in centimeters
-#   - width:      module box width
-#   - height:     module box height
-#   - call:       call statements of module
+#   - see explanation for Object class
 #   - type_stat:  type statements of module
-#   - row:        row placement in grid
-#   - column:     column placement in grid
-#   - path:       path to .f90 file
 # Returns:
 #   - nothing
 # Used by:
-#   - Function for importing attributes(parameters) to module class(object)
+#   - module_class (object)
 #-------------------------------------------------------------------------------
-class Module(object):
-  def __init__(module, type, name, use, var, meth,  \
-               level, x0, y0, width, height,        \
-               call, type_stat, row, column, path):
+class Module(Object):
+  def __init__(self, name, path,              \
+               use, var, meth, call, types):
 
-    module.name      = name
-    module.use       = use
-    module.var       = var
-    module.meth      = meth
-    module.call      = call
-    module.level     = level
-    module.x0        = x0
-    module.y0        = y0
-    module.type      = type
-    module.w         = width
-    module.h         = height
-    module.type_stat = type_stat
-    module.row       = row
-    module.column    = column
-    module.path      = path
+    # Call mother's init
+    Object.__init__(self, name, path,             \
+                    use, var, meth, call, types)
 
--- Objects/Subroutine.py
+++ Objects/Subroutine.py
@@ -1,47 +1,24 @@
+from Objects.Object import Object  # mother
 
 #===============================================================================
-# Defining subroutine class
+# Defining Subroutine class
 #
 # Parameters:
-#   - subroutine: initialize name (so you can write subroutine.name to get name)
-#   - type:       type of object (can be module/subroutine/function/program)
-#   - name:       name of the subroutine
-#   - use:        list of subroutine use statements
-#   - var:        list of subroutine variables
-#   - meth:       list of subroutine methods
-#   - level:      level of subroutine
-#   - x0:         first corner(upper left) position on x axis in centimeters
-#   - y0:         first corner(upper left) position on y axis in centimeters
-#   - width:      subroutine box width
-#   - height:     subroutine box height
-#   - call:       call statements of subroutine
+#   - see explanation for Object class
 #   - type_stat:  type statements of subroutine
-#   - row:        row placement in grid
-#   - column:     column placement in grid
+#   - in_module:  holds the module name it is in
 # Returns:
 #   - nothing
 # Used by:
-#   - Function for importing attributes(parameters) to subroutine class(object)
+#   - subroutine_class (object)
 #-------------------------------------------------------------------------------
-class Subroutine(object):
-  def __init__(subroutine, type, name, use, var, meth,  \
-               level, x0, y0, width, height,            \
-               call, type_stat, row, column, path):
+class Subroutine(Object):
+  def __init__(self, name, path,              \
+               use, var, meth, call, types):
 
-    subroutine.name      = name
-    subroutine.use       = use
-    subroutine.var       = var
-    subroutine.meth      = meth
-    subroutine.call      = call
-    subroutine.level     = level
-    subroutine.x0        = x0
-    subroutine.y0        = y0
-    subroutine.type      = type
-    subroutine.w         = width
-    subroutine.h         = height
-    subroutine.type_stat = type_stat
-    subroutine.row       = row
-    subroutine.column    = column
-    subroutine.path      = path
-    subroutine.in_module = []
+    # Call mother's init
+    Object.__init__(self, name, path,      \
+                    use, var, meth, call, types)
+
+    self.in_module = []
 
