0b48b4648c42e04eaa5deb62b67775f96bcd6095
Niceno <bojan.niceno.scientist@gmail.com>
2020-02-01 08:41:01 +0100

    Corrected color for subroutine.

--- Const.py
+++ Const.py
@@ -1,9 +1,10 @@
 #===============================================================================
 # Module with constants
 #-------------------------------------------------------------------------------
-BOX_MARGINS             = 1.5                  # box margins in cm (presumably)
-UNIT_BOX_HEIGHT         = 1.00                 # unit box height
-THICKNESS               = 2                    # box line thickness
+BOX_MARGINS             =  1.5                 # box margins in cm (presumably)
+UNIT_BOX_HEIGHT         =  1.00                # unit box height
+THICKNESS               =  2                   # box line thickness
+WALK_DIRECTIONS         = 16                   # should be divideable by 4
 FIG_FILE_NAME           = "diagram.fig"        # xfig output file name
 IJ_FILE_NAME            = "diagram.ij"         # file with object i,j coords
 XY_FILE_NAME            = "diagram.xy"         # file with object i,j coords
@@ -12,7 +13,7 @@ FONT_HEADER             = "Courier-Bold"       # font for headers
 FONT_NORMAL             = "Courier"            # font for everything else
 COLOR_BOX               = "White"              # color of var,meth and use boxes
 COLOR_HEADER_MODULE     = "LtBlue"             # color of module header
-COLOR_HEADER_SUBROUTINE = "Pink2"              # color of subroutine header
+COLOR_HEADER_SUBROUTINE = "Pink3"              # color of subroutine header
 COLOR_HEADER_FUNCTION   = "Yellow"             # color of function header
 COLOR_HEADER_PROGRAM    = "Green2"             # color of program header
 COLOR_GRID              = "Magenta"
